9c474507d13ca88d7abe321d7796215726c1d55d
Tony Lee <tonyh.lee@yahoo.com>
2020-03-09 12:12:29 -0700

    switch chrome and firefox

--- tests/ui/ui_tester.py
+++ tests/ui/ui_tester.py
@@ -36,17 +36,17 @@ class UITester(ABC):
             if args.headless:
                 browser_options.add_argument('--headless')
 
-        # Test Firefox
-        options = FirefoxOptions()
-        add_headless(options)
-        self.browser = webdriver.Firefox(log_path='', firefox_options=options)
-        self.test()
-        self.browser.close()
-
         # Test Chrome
         options = ChromeOptions()
         add_headless(options)
         self.browser = webdriver.Chrome(chrome_options=options)
+        self.test()
+        self.browser.close()
+
+        # Test Firefox
+        options = FirefoxOptions()
+        add_headless(options)
+        self.browser = webdriver.Firefox(log_path='', firefox_options=options)
         self.test()
         self.browser.close()
 
